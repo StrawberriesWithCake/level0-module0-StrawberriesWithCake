ee172ea413b8a2ca0424036db102c7d1c5ac4212
League Student <league@iMac-38.attlocal.net>
2023-08-09 11:54:26 -0700

    8-9 changes

--- src/_03_print_and_popups/_2_madlibs/MadlibsOfTheAmazon.java
+++ src/_03_print_and_popups/_2_madlibs/MadlibsOfTheAmazon.java
@@ -9,30 +9,30 @@ public class MadlibsOfTheAmazon {
 		String prompt =  "If you find yourself having to cross a piranha-infested river, here's how to do it...";
 		
 		// Get the player to enter an adjective
-
+		String Adjective = JOptionPane.showInputDialog("Enter an adjective");
 		// Get the player to enter a type of liquid
-
+		String Liquid = JOptionPane.showInputDialog("Enter a type of liquid");
 		// Get the player to enter a body part
-
+		String BodyPart = JOptionPane.showInputDialog("Enter a type of body part");
 		// Get the player to enter a verb
-
-		// Get the player to enter a place
-
+		String Verb = JOptionPane.showInputDialog("Enter a verb");
+		// Get the player to enter a location
+		String Location = JOptionPane.showInputDialog("Enter a location");
 		// The story below has has been written as a group of Strings joined together by + signs.
 		// The story contains place holders, indicated by [** **] which you need to replace with
 		// the values entered by the player.
 		// Hint:  You will need to add more + signs to join the variables to the other parts of the story.
 		
 		String story = 
-		"Piranhas are more [**adjective**] during the day, so cross the river at\n"  +
-		"night. Piranhas are attracted to fresh [**type of liquid**] and will most\n"  +
-		"likely take a bite out of your [**body part**] if you [**verb**]. Whatever\n"  +
+		"Piranhas are more " + Adjective + " during the day, so cross the river at\n"  +
+		"night. Piranhas are attracted to fresh " + Liquid + " and will most\n"  +
+		"likely take a bite out of your " + BodyPart + " if you " + Verb + ". Whatever\n"  +
 		"you do, if you have an open wound, try to find another way to get\n"  +
-		"back to the [**place**]. Good luck!";
+		"back to the " + Location + ". Good luck!";
 		
 		// Make a pop-up that contains the final story. The \n escape characters add line breaks to the story. 
 		// If you need to, move them around to make your story look better in the pop-up
-		
+		JOptionPane.showMessageDialog(null, story);
 		// If you want to write your own Madlib story, just change the story variable and ask the player different questions.
 
 	}
